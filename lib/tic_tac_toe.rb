ac017e5264bd7c347c0936ee1c2b0d76fa71024a
Ryan M <rmccauleymusic@gmail.com>
2019-11-15 02:32:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -97,13 +97,16 @@ end
   end
 
   def winner
-    if won?
-      @board[won?[0]] == "X" ? "X" : "O"
+    if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+      return "X"
+    elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+      return "O"
     else
       nil
     end
   end
 
+  
 end
 
 #game =TicTacToe.new
